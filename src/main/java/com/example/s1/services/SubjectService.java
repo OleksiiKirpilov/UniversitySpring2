86bd36aad8fc6e08ff04ab0be1233157c350f6d8
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-09-27 19:44:37 +0300

    52% test coverage

--- src/main/java/com/example/s1/services/SubjectService.java
+++ src/main/java/com/example/s1/services/SubjectService.java
@@ -11,7 +11,6 @@ import com.example.s1.utils.Path;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
